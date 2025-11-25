868dea5a93cc36fa50e2116a6606354224780e2e
Pavel Rakhmanov <avedomaru@gmail.com>
2025-11-25 20:02:26 +0100

    refactor(demo-app): better QuoteTrack impl with parallel statuses displaying support

--- examples/next-js-app/components/QuoteTrack.tsx
+++ examples/next-js-app/components/QuoteTrack.tsx
@@ -4,10 +4,10 @@ import {
   Blockchain,
   type Quote,
   type TradeStatus,
-  useTrackTrade,
+  useOmniston,
 } from "@ston-fi/omniston-sdk-react";
 import { useTonAddress } from "@tonconnect/ui-react";
-
+import { useEffect, useState } from "react";
 import { Spinner } from "@/components/ui/spinner";
 import { useOutgoingTxHash } from "@/hooks/useOutgoingTxHash";
 import { cn } from "@/lib/utils";
@@ -69,20 +69,44 @@ export const QuoteTrack = withQuoteTrackProps(
     walletAddress,
     ...props
   }: QuoteTrackProps & { className?: string }) => {
-    const { data: tradeStatus } = useTrackTrade({
-      quoteId,
-      outgoingTxHash,
-      traderWalletAddress: {
-        address: walletAddress,
-        blockchain: Blockchain.TON,
-      },
-    });
+    const [tradeStatuses, setTradeStatuses] = useState<TradeStatus[]>([]);
 
-    if (!tradeStatus?.status) return null;
+    const omniston = useOmniston();
+
+    useEffect(() => {
+      const trackTradeParams = {
+        quoteId,
+        outgoingTxHash,
+        traderWalletAddress: {
+          address: walletAddress,
+          blockchain: Blockchain.TON,
+        },
+      };
+
+      const subscription = omniston.trackTrade(trackTradeParams).subscribe({
+        next: (status) => {
+          setTradeStatuses((prevStatuses) => [...prevStatuses, status]);
+        },
+        error: (error) => {
+          //
+        },
+      });
+
+      return () => {
+        subscription.unsubscribe();
+      };
+    }, [omniston, quoteId, outgoingTxHash, walletAddress]);
+
+    const tradeStatus = tradeStatuses[tradeStatuses.length - 1];
+
+    if (!tradeStatus) {
+      return null;
+    }
 
     return (
       <div className={cn("p-4 border rounded-md spaced-y-2", props.className)}>
-        <TradeStatusContent quoteId={quoteId} status={tradeStatus.status} />
+        <TradeStatusesPresenter quoteId={quoteId} statuses={tradeStatuses} />
+
         <div className="flex gap-2 items-center">
           <span>Transfer timestamp:</span>
           <pre>
@@ -102,51 +126,68 @@ export const QuoteTrack = withQuoteTrackProps(
   },
 );
 
-function TradeStatusContent({
+function TradeStatusesPresenter({
   quoteId,
-  status,
+  statuses,
 }: {
   quoteId: string;
-  status: NonNullable<TradeStatus["status"]>;
+  statuses: TradeStatus[];
 }) {
-  if (status.unsubscribed) {
+  const unsubscribed = findStatus(statuses, "unsubscribed");
+
+  if (unsubscribed) {
     return <span>Request timed out</span>;
   }
 
-  if (status.awaitingTransfer) {
-    return <span>Awaiting Transfer...</span>;
+  const tradeSettled = findStatus(statuses, "tradeSettled");
+
+  if (tradeSettled) {
+    return (
+      <span className="inline-flex gap-2 items-center">
+        <span>Trade Settled:</span>
+        <pre>{tradeSettled.result}</pre>
+      </span>
+    );
   }
 
-  if (status.transferring) {
+  const receivingFunds = findStatus(statuses, "receivingFunds");
+
+  if (receivingFunds) {
     return (
       <span className="inline-flex gap-2 items-center">
         <Spinner />
-        <span>Transferring...</span>
+        <span>Receiving funds...</span>
       </span>
     );
   }
 
-  if (status.awaitingFill) {
+  const awaitingFill = findStatus(statuses, "awaitingFill");
+  const refundAvailableStatus = findStatus(statuses, "refundAvailable");
+
+  if (awaitingFill || refundAvailableStatus) {
     return (
-      <span className="inline-flex gap-2 items-center">
-        <Spinner />
-        <span>Waiting for trade to be filled...</span>
-      </span>
+      <>
+        {awaitingFill && (
+          <span className="inline-flex gap-2 items-center">
+            <Spinner />
+            <span>Waiting for trade to be filled...</span>
+          </span>
+        )}
+        {refundAvailableStatus && (
+          <span className="inline-flex gap-2 items-center">
+            <span>Refund available</span>
+            <WithdrawButton size="sm" quoteId={quoteId}>
+              Withdraw
+            </WithdrawButton>
+          </span>
+        )}
+      </>
     );
   }
 
-  if (status.refundAvailable) {
-    return (
-      <span className="inline-flex gap-2 items-center">
-        <span>Refund available</span>
-        <WithdrawButton size="sm" quoteId={quoteId}>
-          Withdraw
-        </WithdrawButton>
-      </span>
-    );
-  }
+  const swapping = findStatus(statuses, "swapping");
 
-  if (status.swapping) {
+  if (swapping) {
     return (
       <span className="inline-flex gap-2 items-center">
         <Spinner />
@@ -155,18 +196,35 @@ function TradeStatusContent({
     );
   }
 
-  if (status.tradeSettled) {
+  const transferring = findStatus(statuses, "transferring");
+
+  if (transferring) {
     return (
       <span className="inline-flex gap-2 items-center">
-        <span>Trade Settled:</span>
-        <pre>{status.tradeSettled.result}</pre>
+        <Spinner />
+        <span>Transferring...</span>
       </span>
     );
   }
 
-  return (
-    <span className="inline-flex gap-2 items-center">
-      <pre>{JSON.stringify(status, null, 2)}</pre>
-    </span>
+  const awaitingTransfer = findStatus(statuses, "awaitingTransfer");
+
+  if (awaitingTransfer) {
+    return <span>Awaiting Transfer...</span>;
+  }
+
+  return null;
+}
+
+function findStatus<K extends keyof NonNullable<TradeStatus["status"]>>(
+  statuses: TradeStatus[],
+  statusKey: K,
+): NonNullable<NonNullable<TradeStatus["status"]>[K]> | undefined {
+  const found = statuses.find(
+    ({ status }) => status?.[statusKey] !== undefined,
   );
+
+  return found?.status?.[statusKey] as
+    | NonNullable<NonNullable<TradeStatus["status"]>[K]>
+    | undefined;
 }
